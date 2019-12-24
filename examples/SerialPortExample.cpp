f384cb127927d887bf0bf5874dd6b0282b527b25
Potapov Sergei <potapov.sa@carddex.ru>
2019-12-24 18:37:05 +0300

    [*] Implemented serial port for WIN

--- examples/SerialPortExample.cpp
+++ examples/SerialPortExample.cpp
@@ -1,10 +1,20 @@
+/**
+ * Blocking serial port example
+ */
+
 #include "../couriercxx/connector/serialport/SerialPortBase.h"
 #include "../couriercxx/logger/Log.h"
 
 int main(int ac, char** av) {
 	Log::setAppName(&av[0][2]);
 
-	SerialPortBase port("/dev/ttyUSB0", 9600);
+	if (ac < 3) {
+		Log::info("\r\nUsage:\r\n%s port_name port_baudrate", &av[0][2]);
+
+		return 0;
+	}
+
+	SerialPortBase port(av[1], atoi(av[2]));
 
 	if (port.open() == -1) {
 		Log::error("SerialPortBase.open() error");
