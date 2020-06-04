8b543ac5b80b69e893e4efe83b9bf86225eb469c
Potapov Sergei <potapov.sa@carddex.ru>
2020-06-04 20:07:59 +0300

    [*]

--- Debug/couriercxx/network/udp/subdir.mk
+++ Debug/couriercxx/network/udp/subdir.mk
@@ -20,7 +20,7 @@ CPP_DEPS += \
 couriercxx/network/udp/%.o: ../couriercxx/network/udp/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: Cross G++ Compiler'
-	g++ -std=c++1y -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	g++ -std=c++1y -O0 -g3 -Wall -c -fmessage-length=0 -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
